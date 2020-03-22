882d579a24d4dc6c63cc4bfc5d67ece3070960d9
Csaba Puskás <puskasc6@gmail.com>
2020-03-22 13:37:22 +0100

    Finished Lesson 5 Homework

--- Lesson 5/Solution/index.js
+++ Lesson 5/Solution/index.js
@@ -1,9 +1,13 @@
 import './style.css';
 const products = require('./products.json');
-console.log(products[0]);
 
-let table = document.querySelector("table");
+let table = document.querySelector("tbody");
 let data =Object.keys(products[0]);
+var details =document.getElementsByClassName("product-details");
+var childNodes = details[0].childNodes;
+
+console.log(products[0]);
+console.log(childNodes);
 
 function generateTable(table,data){
   for(let element of data){
@@ -32,3 +36,68 @@ function generateTable(table,data){
 }
 
 generateTable(table,products);
+
+function addRowHandlers() {
+    var table = document.getElementsByTagName("table");
+    var rows = table[0].rows;
+    for (let i = 1; i < rows.length; i++) {
+        let row = rows[i];
+        row.addEventListener('click',showProductDetails);
+    }
+}
+
+function generateDetails(mouseEvent){
+  let row = mouseEvent.toElement.parentElement.rowIndex-1; //here we find which row the user clicked; -1 because array start at 0
+  console.log(row+'. row');
+   let divCount=0;//we'll count which div are we in
+   let pCount=0; //we'll count which paragraph are we in
+  for(let i=0;i<childNodes.length;i++){
+   switch(childNodes[i].nodeName){//there are comments in the html which are considered as nodes (?)
+    case "P" :fillDetailsP(row,i,pCount); //row: we need to pass which row need to be described,i: the index of the child in the product details container
+              pCount+=1;
+              break;
+    case "DIV" :fillDetailsDiv(row,i,divCount);//same but for the divs
+                divCount+=1;
+                break;
+    default:break;
+    }
+  }
+  
+}
+
+function fillDetailsDiv(row,i,divCount){
+  if (divCount){
+    for (let index=0;index<products[row].images.length;index++){
+    var img=document.createElement("img");
+    img.src=products[row].images[index];
+    console.log(img);
+    childNodes[i].appendChild(img);
+    }
+  
+
+  }else{childNodes[i].innerHTML = "Categories: "+products[row].categories;}
+}
+
+function fillDetailsP(row,i,pCount){
+  switch (pCount){
+    case 0 :childNodes[i].innerHTML = "Product Name: "+products[row].name;break;
+    case 1 :childNodes[i].innerHTML = "Product Description: " +products[row].description;break;
+    case 2 :childNodes[i].innerHTML = "Price:"+products[row].price;break;
+    case 3 :childNodes[i].innerHTML = "Quantity: "+products[row].quantity;break;
+  }
+  
+
+}
+
+function emptyProductDetails(){
+  console.log("Emptied img container");
+  childNodes[13].innerHTML='';
+}
+
+function showProductDetails(mouseEvent){
+  emptyProductDetails();
+  generateDetails(mouseEvent);
+}
+
+addRowHandlers();
+
