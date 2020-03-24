e850d3df26339d7007a91d2dc564f535bef8410d
Csaba Puskás <puskasc6@gmail.com>
2020-03-24 16:29:35 +0100

    Implemented filter by categories

--- Lesson 6/src/ui/assets/js/product/services/products.js
+++ Lesson 6/src/ui/assets/js/product/services/products.js
@@ -2,7 +2,7 @@ var ProductService = (function(ProductItem){
     var url = '/api/products';
 
     function searchProduct(searchParameters){
-        //decide whether the user is searching using the form or e searching by using a filter    
+        //decide whether the user is searching using the form or searching by using a filter    
         if (searchParameters.category){ 
 
             var categoryUrl=url+'/category/'+encodeURIComponent(searchParameters.category);
@@ -37,7 +37,7 @@ var ProductService = (function(ProductItem){
         var query = [];
         var params = searchParams.toJSON();
         //console.log(searchParams);    i tought i could extend the existing search with searching by category also
-        //console.log("afterjson");     but here the tojson() i couldnt figure out why ignores the category in the object
+        //console.log("afterjson");     but here the tojson() i couldnt figure out why tojson ignores the category in the object
         //console.log(params);          in the json category does not appear
         Object.keys(params).forEach(function(param){
             if (params[param]) {
